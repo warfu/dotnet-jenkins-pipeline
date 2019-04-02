fb4c8d37bf43b1d50a229f7e529c11e12b5786a3
wangf13 <wangf13@mingyuanyun.com>
2019-04-02 09:20:33 +0800

    修改build

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@
 set -x
 
 #docker info
+#docker version
 
-docker version
+#cp ./Solution1/src/WebApplication1/Dockerfile  Solution1/src/WebApplication1/bin/Release/netcoreapp2.2
+
+cd  ./Solution1/src/
+
+docker build -t Solution1:v1.0 .
 
 set +x
