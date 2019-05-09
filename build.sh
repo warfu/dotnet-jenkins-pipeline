1baf068381f72a3048935038d7bec80168da0769
wangf13 <wangf13@mingyuanyun.com>
2019-05-09 15:08:40 +0800

    提交build

--- build.sh
+++ build.sh
@@ -6,17 +6,24 @@ set -x
 #docker info
 #docker version
 
-cp ./Solution1/src/WebApplication1/Dockerfile  ./Solution1/src/WebApplication1/bin/Release/netcoreapp2.2
+#cp ./Solution1/src/WebApplication1/Dockerfile  ./Solution1/src/WebApplication1/bin/Release/netcoreapp2.2
 
 
 #cd  ./Solution1/src/
 
-cd ./Solution1/src/WebApplication1/bin/Release/
+#cd ./Solution1/src/WebApplication1/bin/Release/
 
 
 
-docker build -t webapplication1:v1.0 .
+#docker build -t webapplication1:v1.0 .
 
 
+ 
+ echo ${JOB_NAME}
+
+
+if [ "${JOB_NAME}" = "yfk-net-agent" ];then
+    echo "ПаµИ"
+fi
 
 set +x
